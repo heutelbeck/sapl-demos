f9c5fe2b4a5bb7ca5aa5fbb73f42e7b782ccfe07
joe-baudisch <Joe.Baudisch@gmail.com>
2018-01-20 16:09:14 +0100

    new submodule

---
+++ sapl-demo-permeval/src/main/java/io/sapl/peembedded/SaplDemoPeEmbeddedApplication.java
@@ -0,0 +1,12 @@
+package io.sapl.peembedded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaplDemoPeEmbeddedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaplDemoPeEmbeddedApplication.class, args);
+	}
+}
