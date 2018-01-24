ae54ef82274361589c5052c3712e03d2bcdd5382
DanielSchmidt <dants@gmx.net>
2018-01-24 19:09:18 +0100

    added filter chain project stub

---
+++ sapl-demo-filterchain/src/test/java/io/sapl/peembedded/SaplDemoPeEmbeddedApplicationTests.java
@@ -0,0 +1,16 @@
+package io.sapl.peembedded;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaplDemoPeEmbeddedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
