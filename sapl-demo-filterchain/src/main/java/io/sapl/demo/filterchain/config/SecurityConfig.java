92d3494101e2183d84430354465fff134c4d4dc0
DanielSchmidt <dants@gmx.net>
2018-01-28 15:44:57 +0100

    initial migration

--- sapl-demo-filterchain/src/main/java/io/sapl/demo/filterchain/config/SecurityConfig.java
+++ sapl-demo-filterchain/src/main/java/io/sapl/demo/filterchain/config/SecurityConfig.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-@EnableWebSecurity(debug = false)
+@EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
--- sapl-demo-filterchain/src/main/java/io/sapl/demo/filterchain/controller/UIController.java
+++ sapl-demo-filterchain/src/main/java/io/sapl/demo/filterchain/controller/UIController.java
@@ -19,7 +19,7 @@ import io.sapl.api.pdp.Response;
 import io.sapl.demo.domain.Patient;
 import io.sapl.demo.domain.resource.PatientResource;
 import io.sapl.demo.repository.PatientenRepo;
-import io.sapl.spring.PolicyEnforcementPoint;
+import io.sapl.spring.StandardSAPLAuthorizator;
 import io.sapl.spring.marshall.Resource;
 import io.sapl.spring.marshall.Subject;
 import io.sapl.spring.marshall.action.HttpAction;
@@ -36,12 +36,12 @@ public class UIController {
 	private static final String REDIRECT_PROFILES = "redirect:profiles";
 	private static final String UPDATE = "update";
 
-	private PolicyEnforcementPoint pep;
+	private StandardSAPLAuthorizator pep;
 
 	private PatientenRepo patientenRepo;
 
 	@Autowired
-	public UIController(PolicyEnforcementPoint pep, PatientenRepo patientenRepo) {
+	public UIController(StandardSAPLAuthorizator pep, PatientenRepo patientenRepo) {
 		this.pep = pep;
 		this.patientenRepo = patientenRepo;
 		LOGGER.debug("created instancewith PolicyEnforcementPoint (!= null:{}) and PatientenRepo (!= null:{})",
