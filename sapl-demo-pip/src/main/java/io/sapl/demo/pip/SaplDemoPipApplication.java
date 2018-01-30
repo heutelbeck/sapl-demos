22682274115b12d6754abb3de10ddd1cd4a56899
Benedikt Heinrich <benedikt.heinrich@web.de>
2018-01-30 16:12:13 +0100

    Adding PIP Demo Project

---
+++ sapl-demo-pip/src/main/java/io/sapl/demo/pip/SaplDemoPipApplication.java
@@ -0,0 +1,12 @@
+package io.sapl.demo.pip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaplDemoPipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaplDemoPipApplication.class, args);
+	}
+}
