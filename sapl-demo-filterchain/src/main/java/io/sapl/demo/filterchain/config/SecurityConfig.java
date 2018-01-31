b295063dca512052c11395c37fff8a0df9643334
Benedikt Heinrich <benedikt.heinrich@web.de>
2018-01-31 22:09:42 +0100

    filterchain project update fixed

--- sapl-demo-filterchain/src/main/java/io/sapl/demo/filterchain/config/SecurityConfig.java
+++ sapl-demo-filterchain/src/main/java/io/sapl/demo/filterchain/config/SecurityConfig.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity //(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
