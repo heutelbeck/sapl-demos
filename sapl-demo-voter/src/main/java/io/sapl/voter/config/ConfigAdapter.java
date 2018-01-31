391b0015417df47ef9b9622575e10f87b5451d31
Daniel Wolfschmidt <DanielWolfschmidt@gmx.de>
2018-01-31 21:54:06 +0100

    addded new voter project and added test for JWT

---
+++ sapl-demo-voter/src/main/java/io/sapl/voter/config/ConfigAdapter.java
@@ -0,0 +1,39 @@
+package io.sapl.voter.config;
+
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ConfigAdapter extends WebSecurityConfigurerAdapter {
+
+	private final AccessDecisionManager adm;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		LOGGER.trace("start configuring...");
+		http
+				.httpBasic().and()
+				.authorizeRequests()
+					.anyRequest().fullyAuthenticated()
+					.accessDecisionManager(adm)
+				.and()
+				.formLogin()
+					.loginPage("/login").permitAll()
+				.and()
+					.logout().logoutUrl("/logout").logoutSuccessUrl("/login").permitAll()
+				.and()
+					.httpBasic()
+				.and()
+					.csrf().disable()
+				;
+	}
+
+		
+	
+	
+}
