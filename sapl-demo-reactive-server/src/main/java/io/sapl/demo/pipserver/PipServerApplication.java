28d45a557761ac0688d6551436e158e3d8994055
Felix Siegrist <felix.siegrist@inventage.com>
2019-01-18 22:46:07 +0100

    Add demo for usage of reactive remote PIP

---
+++ sapl-demo-reactive-server/src/main/java/io/sapl/demo/pipserver/PipServerApplication.java
@@ -0,0 +1,13 @@
+package io.sapl.demo.pipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipServerApplication.class, args);
+    }
+
+}
--- sapl-demo-reactive/src/main/java/io/sapl/demo/SaplDemoReactiveApplication.java
+++ sapl-demo-reactive/src/main/java/io/sapl/demo/SaplDemoReactiveApplication.java
@@ -1,18 +1,18 @@
 package io.sapl.demo;
 
-import io.sapl.demo.domain.DemoData;
-import io.sapl.demo.domain.PatientRepo;
-import io.sapl.demo.domain.RelationRepo;
-import io.sapl.demo.domain.UserRepo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.sapl.demo.domain.DemoData;
+import io.sapl.demo.domain.PatientRepo;
+import io.sapl.demo.domain.RelationRepo;
+import io.sapl.demo.domain.UserRepo;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableJpaRepositories("io.sapl.demo.domain")
@@ -27,8 +27,7 @@ public class SaplDemoReactiveApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demoData(UserRepo userRepo, PatientRepo patientRepo, RelationRepo relationRepo,
-			ApplicationContext ctx) {
+	public CommandLineRunner demoData(UserRepo userRepo, PatientRepo patientRepo, RelationRepo relationRepo) {
 		return args -> DemoData.loadDemoDataset(userRepo, defaultPassword, patientRepo, relationRepo);
 	}
 }
