bf343aa9b3e2a73055d33c16250f128cd35ad71c
Felix Siegrist <felix.siegrist@inventage.com>
2019-02-11 23:32:09 +0100

    Add endpoint for PatientPIP to sapl-demo-reactive-server to make the PatientPIP accessible from remote PDP

---
+++ sapl-demo-reactive-server/src/main/java/io/sapl/demo/pipserver/PatientController.java
@@ -0,0 +1,25 @@
+package io.sapl.demo.pipserver;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
+import io.sapl.demo.shared.pip.PatientPIP;
+
+@RestController
+@RequestMapping("rest/patient")
+public class PatientController {
+
+    @GetMapping(value = "related/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public JsonNode getRelations(@PathVariable String id) {
+        final PatientPIP patientPIP = new PatientPIP();
+        final JsonNode relations = patientPIP.getRelations(JsonNodeFactory.instance.textNode(id), null);
+        return relations;
+    }
+
+}
--- sapl-demo-reactive-server/src/main/java/io/sapl/demo/pipserver/PipServerApplication.java
+++ sapl-demo-reactive-server/src/main/java/io/sapl/demo/pipserver/PipServerApplication.java
@@ -1,13 +1,32 @@
 package io.sapl.demo.pipserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+import io.sapl.demo.domain.DemoData;
+import io.sapl.demo.domain.PatientRepo;
+import io.sapl.demo.domain.RelationRepo;
+import io.sapl.demo.domain.UserRepo;
+import io.sapl.springboot.autoconfig.PDPAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, PDPAutoConfiguration.class})
+@EnableJpaRepositories("io.sapl.demo.domain")
+@EntityScan("io.sapl.demo.domain")
+@ComponentScan({"io.sapl.demo.pipserver", "io.sapl.demo.shared.pip"})
 public class PipServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PipServerApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demoData(UserRepo userRepo, PatientRepo patientRepo, RelationRepo relationRepo) {
+        return args -> DemoData.loadDemoDataset(userRepo, "", patientRepo, relationRepo);
+    }
 }
