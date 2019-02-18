c76d763a8b31beac66155f9b0f4232a4348b7e0f
Felix Siegrist <felix.siegrist@inventage.com>
2019-02-18 22:23:59 +0100

    Workaround for problem with non existing Vaadin session in unit test

--- sapl-demo-reactive/src/main/java/org/demo/security/VaadinSessionSecurityContextHolderStrategy.java
+++ sapl-demo-reactive/src/main/java/org/demo/security/VaadinSessionSecurityContextHolderStrategy.java
@@ -1,23 +1,39 @@
 package org.demo.security;
 
-import com.vaadin.server.VaadinSession;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.core.context.SecurityContextImpl;
+
+import com.vaadin.server.VaadinSession;
 
 /**
  * A custom {@link SecurityContextHolderStrategy} that stores the {@link SecurityContext} in the Vaadin Session.
  */
 public class VaadinSessionSecurityContextHolderStrategy implements SecurityContextHolderStrategy {
 
+    private static boolean isUsedInUnitTest;
+
+    private static SecurityContext securityContextForUnitTest;
+
+    public static void initForUnitTest() {
+        isUsedInUnitTest = true;
+        securityContextForUnitTest = new SecurityContextImpl();
+    }
+
     @Override
     public void clearContext() {
-        getSession().setAttribute(SecurityContext.class, null);
+        if (!isUsedInUnitTest) {
+            getSession().setAttribute(SecurityContext.class, null);
+        }
     }
 
     @Override
     public SecurityContext getContext() {
-        VaadinSession session = getSession();
+        if (isUsedInUnitTest) {
+            return securityContextForUnitTest;
+        }
+
+        final VaadinSession session = getSession();
         SecurityContext context = session.getAttribute(SecurityContext.class);
         if (context == null) {
             context = createEmptyContext();
@@ -28,7 +44,9 @@ public class VaadinSessionSecurityContextHolderStrategy implements SecurityConte
 
     @Override
     public void setContext(SecurityContext context) {
-        getSession().setAttribute(SecurityContext.class, context);
+        if (!isUsedInUnitTest) {
+            getSession().setAttribute(SecurityContext.class, context);
+        }
     }
 
     @Override
@@ -37,7 +55,7 @@ public class VaadinSessionSecurityContextHolderStrategy implements SecurityConte
     }
 
     private static VaadinSession getSession() {
-        VaadinSession session = VaadinSession.getCurrent();
+        final VaadinSession session = VaadinSession.getCurrent();
         if (session == null) {
             throw new IllegalStateException("No VaadinSession bound to current thread");
         }
--- sapl-demo-reactive/src/test/java/org/demo/DemoApplicationTest.java
+++ sapl-demo-reactive/src/test/java/org/demo/DemoApplicationTest.java
@@ -1,15 +1,20 @@
 package org.demo;
 
-import org.junit.Ignore;
+import org.demo.security.VaadinSessionSecurityContextHolderStrategy;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class DemoApplicationTest {
+
+	@BeforeClass
+	public static void setUp() {
+		VaadinSessionSecurityContextHolderStrategy.initForUnitTest();
+	}
 
 	@Test
 	public void contextLoads() {
