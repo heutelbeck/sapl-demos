696173212e738a773d02c792151ed1886f6cbfc2
heutelbeck <dheutelbeck@ftk.de>
2019-03-28 22:15:07 +0100

    build fix

--- sapl-demo-reactive/src/main/java/org/demo/service/BackendService.java
+++ sapl-demo-reactive/src/main/java/org/demo/service/BackendService.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Service;
 
 import com.vaadin.ui.Notification;
 
-import io.sapl.spring.annotation.EnforcePolicies;
+import io.sapl.spring.method.pre.PreEnforce;
 
 @Service
 public class BackendService implements Serializable {
 
-    @EnforcePolicies(action = "get", resource = "profiles")
-    public void demonstrateUsageOfPdpAuthorizeAnnotation() {
-        Notification.show("You are authorized to load the list of patients.", Notification.Type.HUMANIZED_MESSAGE);
-    }
+	@PreEnforce(action = "'get'", resource = "'profiles'")
+	public void demonstrateUsageOfPdpAuthorizeAnnotation() {
+		Notification.show("You are authorized to load the list of patients.", Notification.Type.HUMANIZED_MESSAGE);
+	}
 }
