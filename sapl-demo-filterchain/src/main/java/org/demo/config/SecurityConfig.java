7e8da993891b2095b91b18fbe970da479e4407cc
Felix Siegrist <felix.siegrist@inventage.com>
2019-04-21 19:20:59 +0200

    Remove MethodSecurityConfiguration as it is provided by the SAPL auto configuration

--- sapl-demo-filterchain/src/main/java/org/demo/config/SecurityConfig.java
+++ sapl-demo-filterchain/src/main/java/org/demo/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package org.demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +12,6 @@ import io.sapl.spring.PolicyEnforcementFilterPEP;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/**
