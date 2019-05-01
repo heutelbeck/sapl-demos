4501b8546ad2282694ced325ea871041df85e650
Felix Siegrist <felix.siegrist@inventage.com>
2019-05-01 23:48:31 +0200

    Workaround for problem with Jackson2Tokenizer sending each array item to a Flux instead of the whole array

--- sapl-demo-domain/src/main/java/org/demo/pip/PatientPIP.java
+++ sapl-demo-domain/src/main/java/org/demo/pip/PatientPIP.java
@@ -1,10 +1,10 @@
 package org.demo.pip;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.demo.domain.Patient;
 import org.demo.domain.PatientRepository;
 import org.demo.domain.Relation;
 import org.demo.domain.RelationRepository;
@@ -19,6 +19,7 @@ import io.sapl.api.pip.AttributeException;
 import io.sapl.api.pip.PolicyInformationPoint;
 import io.sapl.api.validation.Number;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
 
 @Service
 @RequiredArgsConstructor
@@ -30,20 +31,21 @@ public class PatientPIP {
 	private final PatientRepository patientRepo;
 
 	@Attribute(name = "relatives")
-	public JsonNode getRelations(@Number JsonNode value, Map<String, JsonNode> variables) {
-		List<String> returnList = new ArrayList<>();
-		returnList.addAll(relationRepo.findByPatientid(value.asLong()).stream().map(Relation::getUsername)
-				.collect(Collectors.toList()));
-		return mapper.convertValue(returnList, JsonNode.class);
+	public Flux<JsonNode> getRelations(@Number JsonNode value, Map<String, JsonNode> variables) {
+		final List<Relation> relations = relationRepo.findByPatientid(value.asLong());
+		final List<String> relationNames = relations.stream().map(Relation::getUsername).collect(Collectors.toList());
+		final JsonNode jsonNode = mapper.convertValue(relationNames, JsonNode.class);
+		return Flux.just(jsonNode);
 	}
 
 	@Attribute(name = "patientRecord")
-	public JsonNode getPatientRecord(@Number JsonNode patientId, Map<String, JsonNode> variables) {
+	public Flux<JsonNode> getPatientRecord(@Number JsonNode patientId, Map<String, JsonNode> variables) {
 		try {
-			return mapper.convertValue(patientRepo.findById(patientId.asLong()).orElseThrow(AttributeException::new),
-					JsonNode.class);
+			final Patient patient = patientRepo.findById(patientId.asLong()).orElseThrow(AttributeException::new);
+			final JsonNode jsonNode = mapper.convertValue(patient, JsonNode.class);
+			return Flux.just(jsonNode);
 		} catch (IllegalArgumentException | AttributeException e) {
-			return JsonNodeFactory.instance.nullNode();
+			return Flux.just(JsonNodeFactory.instance.nullNode());
 		}
 	}
 }
--- sapl-demo-pip-server/src/main/java/org/demo/PatientPIPController.java
+++ sapl-demo-pip-server/src/main/java/org/demo/PatientPIPController.java
@@ -8,26 +8,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 
 
 @RestController
 @RequestMapping("rest/patients")
 @RequiredArgsConstructor
+@Slf4j
 public class PatientPIPController {
 
     private final PatientPIP patientPIP;
+    private final ObjectMapper objectMapper;
 
-    @GetMapping(value = "{id}/relatives", produces = MediaType.APPLICATION_JSON_VALUE)
-    public JsonNode getRelations(@PathVariable String id) {
-        return patientPIP.getRelations(JsonNodeFactory.instance.textNode(id), null);
+    @GetMapping(value = "{id}/relatives", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<JsonNode> getRelations(@PathVariable String id) {
+        final Flux<JsonNode> relations = patientPIP.getRelations(JsonNodeFactory.instance.textNode(id), null);
+        // PatientPIP.getRelations(id) returns an ArrayNode containing the names of all relatives.
+        // When ArrayNodes are serialized for a Flux (mediaType=application/stream+json), Spring's
+        // Jackson2Tokenizer passes each array item to the ObjectMapper to serialize and flush it
+        // (see org.springframework.http.codec.json.Jackson2Tokenizer.tokenize(dataBuffers, jsonFactory,
+        // tokenizeArrayElements)).
+        // This results in a stream of strings instead of a stream of arrays containing strings. Expressions
+        // like 'subject.name in pipUrl.<http.get>' in the policies will not work, because 'pipUrl.<http.get>'
+        // is a string, not an array.
+        // As it is not possible to configure the Jackson2Tokenizer to not treat ArrayNodes like this, we
+        // have to work around the problem by wrapping the array with an object before serializing it. The
+        // expressions in the policies must then be adjusted to 'subject.name in pipUrl.<http.get>.relatives'.
+        return relations
+                .map(jsonNode -> {
+                    final ObjectNode objectNode = objectMapper.createObjectNode();
+                    final ArrayNode relatives = objectNode.putArray("relatives");
+                    relatives.addAll((ArrayNode) jsonNode);
+                    return (JsonNode) objectNode;
+                })
+                .doOnNext(jsonNode -> LOGGER.trace("PatientPIPController.getRelations() returns {}", jsonNode));
     }
 
-    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public JsonNode getPatientRecord(@PathVariable String id) {
-        return patientPIP.getPatientRecord(JsonNodeFactory.instance.textNode(id), null);
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<JsonNode> getPatientRecord(@PathVariable String id) {
+        return patientPIP.getPatientRecord(JsonNodeFactory.instance.textNode(id), null)
+                .doOnNext(jsonNode -> LOGGER.trace("PatientPIPController.getPatientRecord() returns {}", jsonNode));
     }
 
 }
