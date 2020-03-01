b9fcaa8997cddda2f0e1a4df1fa09fd53c32cd01
Nico-Pascal Huch <nphuch@gmail.com>
2020-03-01 22:15:55 +0100

    added SaplEditor.DocumentChangedListener

--- sapl-demo-web-editor/src/main/java/io/sapl/web/views/javabasedview/JavabasedViewView.java
+++ sapl-demo-web-editor/src/main/java/io/sapl/web/views/javabasedview/JavabasedViewView.java
@@ -15,8 +15,15 @@ public class JavabasedViewView extends Div {
     public JavabasedViewView() {
         setId("javabased-view-view");
         SaplEditor editor = new SaplEditor();
+        editor.addDocumentChangedListener(new SaplEditor.DocumentChangedListener() {
+			@Override
+			public void onDocumentChanged(String newValue) {
+				System.out.println("value changed: " + newValue);
+				
+			}
+		});
+        
         add(editor);
-        editor.addValueChangeListener(e -> System.out.println("value changed: "+e.isFromClient()+"  '"+e.getValue()+"'"));
         editor.setValue("policy \"set by Vaadin View after instantiation ->\\u2588<-\" permit");
     }
 
