370aa3c03f3069b7b3a46392889374cdefbca9af
Nico-Pascal Huch <nphuch@gmail.com>
2020-03-16 00:03:00 +0100

    removed flux, added event listener

--- sapl-demo-web-editor/src/main/java/io/sapl/web/views/javabasedview/JavabasedViewView.java
+++ sapl-demo-web-editor/src/main/java/io/sapl/web/views/javabasedview/JavabasedViewView.java
@@ -5,13 +5,15 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import io.sapl.vaadin.DocumentChangedEvent;
+import io.sapl.vaadin.DocumentChangedListener;
 import io.sapl.vaadin.SaplEditor;
 import io.sapl.vaadin.SaplEditorConfiguration;
 import io.sapl.web.MainView;
 @Route(value = "", layout = MainView.class)
 @PageTitle("Java-based View")
 @CssImport("styles/views/javabasedview/javabased-view-view.css")
-public class JavabasedViewView extends Div {
+public class JavabasedViewView extends Div implements DocumentChangedListener {
 
     public JavabasedViewView() {
         setId("javabased-view-view");
@@ -21,14 +23,15 @@ public class JavabasedViewView extends Div {
         config.TextUpdateDelay = 2000;
         
         SaplEditor editor = new SaplEditor(config);
-        editor.getDocumentChangedFlux().subscribe(this::onEditorDocumentChanged);
+        editor.addListener(this);
         
         add(editor);
         editor.setValue("policy \"set by Vaadin View after instantiation ->\\u2588<-\" permit");
     }
     
-    public void onEditorDocumentChanged(String newValue) {
-    	System.out.println("value changed: " + newValue);
-    }
+	@Override
+	public void onDocumentChanged(DocumentChangedEvent event) {
+		System.out.println("value changed: " + event.getNewValue());
+	}
 
 }
