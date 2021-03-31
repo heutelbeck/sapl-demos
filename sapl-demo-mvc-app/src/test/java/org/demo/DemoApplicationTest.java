83a0cc87480d99a6057b004b388f90cc1279eb92
heutelbeck <dheutelbeck@ftk.de>
2021-04-01 01:37:35 +0200

    Fix reactive demo

--- sapl-demo-mvc-app/src/test/java/org/demo/DemoApplicationTest.java
+++ sapl-demo-mvc-app/src/test/java/org/demo/DemoApplicationTest.java
@@ -1,12 +1,9 @@
 package org.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class DemoApplicationTest {
 
 	@Test
