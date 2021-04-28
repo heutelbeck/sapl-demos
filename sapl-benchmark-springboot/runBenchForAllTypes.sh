81ce82285d4d3425ff6924959444783ee2539bfd
Marc Lucas Baur <mlbaur@icloud.com>
2021-04-28 17:14:33 +0200

    new property files

--- sapl-benchmark-springboot/runBenchForAllTypes.sh
+++ sapl-benchmark-springboot/runBenchForAllTypes.sh
@@ -3,17 +3,18 @@
 java --version
 
 #for i in {1..10}
-#do
+#do∞
 	SEED=3242
 	RUNS=100
 	ITER=1
 
 	echo "IMPROVED - $SEED"
-	time java -Xms4096m -Xmx14000m -jar target/sapl-benchmark-springboot-1.0.0-SNAPSHOT.jar -index IMPROVED -iter $ITER --sapl.random.seed=$SEED --sapl.number.of.benchmark.runs=$RUNS
-	
+		time java -Xms4096m -Xmx14000m -Dspring.profiles.active=1000 -jar target/sapl-benchmark-springboot-1.0.0-SNAPSHOT.jar \
+		-index IMPROVED -iter $ITER --sapl.random.seed=23523 --sapl.number.of.benchmark.runs=$RUNS
 
 	echo "SIMPLE - $SEED"
-	time java -Xms4096m -Xmx14000m -jar target/sapl-benchmark-springboot-1.0.0-SNAPSHOT.jar -index SIMPLE -iter $ITER --sapl.random.seed=$SEED --sapl.number.of.benchmark.runs=$RUNS
+		time java -Xms4096m -Xmx14000m -Dspring.profiles.active=1000 -jar target/sapl-benchmark-springboot-1.0.0-SNAPSHOT.jar \
+		-index SIMPLE -iter $ITER --sapl.random.seed=23523 --sapl.number.of.benchmark.runs=$RUNS
 
 	#done
 
