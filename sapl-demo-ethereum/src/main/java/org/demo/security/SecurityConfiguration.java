213f07c36ab051c76e159a67cc73a7fb05367a53
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2022-06-15 18:24:28 +0200

    Update SecurityConfiguration.java

--- sapl-demo-ethereum/src/main/java/org/demo/security/SecurityConfiguration.java
+++ sapl-demo-ethereum/src/main/java/org/demo/security/SecurityConfiguration.java
@@ -15,18 +15,16 @@
  */
 package org.demo.security;
 
-import org.demo.domain.PrinterUserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
 	private static final String LOGIN_PROCESSING_URL = "/login";
 
@@ -36,35 +34,55 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	private static final String LOGOUT_SUCCESS_URL = "/login";
 
-	@Autowired
-	private PrinterUserService printerUserService;
-
 	/**
 	 * Require login to access internal pages and configure login form.
 	 */
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
 
-		http.csrf().disable().requestCache().requestCache(new CustomRequestCache()).and().authorizeRequests()
-				.requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll().anyRequest().authenticated()
-				.and().formLogin().loginPage(LOGIN_URL).permitAll().loginProcessingUrl(LOGIN_PROCESSING_URL)
-				.failureUrl(LOGIN_FAILURE_URL).and().logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL)
-				.invalidateHttpSession(true);
-	}
-
-	@Override
-	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(printerUserService);
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// @formatter:off
+		return http  .csrf()
+				     .disable()
+				     .requestCache()
+				     .requestCache(new CustomRequestCache())
+			   .and().authorizeRequests()
+		             .requestMatchers(SecurityUtils::isFrameworkInternalRequest)
+		             .permitAll()
+		             .anyRequest()
+		             .authenticated()
+		       .and().formLogin()
+		             .loginPage(LOGIN_URL)
+		             .permitAll()
+		             .loginProcessingUrl(LOGIN_PROCESSING_URL)
+		             .failureUrl(LOGIN_FAILURE_URL)
+		       .and().logout()
+		             .logoutSuccessUrl(LOGOUT_SUCCESS_URL)
+		             .invalidateHttpSession(true)
+		       .and().build();
+		// @formatter:on
 	}
 
 	/**
 	 * Allows access to static resources, bypassing Spring security.
 	 */
-	@Override
-	public void configure(WebSecurity web) {
-		web.ignoring().antMatchers("/VAADIN/**", "/favicon.ico", "/robots.txt", "/manifest.webmanifest", "/sw.js",
-				"/offline-page.html", "/icons/**", "/images/**", "/frontend/**", "/webjars/**", "/h2-console/**",
-				"/frontend-es5/**", "/frontend-es6/**");
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		// @formatter:off
+		return (web) -> web.ignoring()
+				           .antMatchers("/VAADIN/**", 
+				        		        "/favicon.ico", 
+				        		        "/robots.txt", 
+				        		        "/manifest.webmanifest", 
+				        		        "/sw.js",
+				        		        "/offline-page.html", 
+				        		        "/icons/**", 
+				        		        "/images/**", 
+				        		        "/frontend/**", 
+				        		        "/webjars/**", 
+				        		        "/h2-console/**",
+				        		        "/frontend-es5/**", 
+				        		        "/frontend-es6/**");
+		// @formatter:on
 	}
 
 }
