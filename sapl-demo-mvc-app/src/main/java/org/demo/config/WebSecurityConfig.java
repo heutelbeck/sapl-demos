7c6ecf1364ff2357acd97b04905e45005a371a0e
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2022-06-15 16:33:26 +0200

    add encoding, cleanup imports

--- sapl-demo-mvc-app/src/main/java/org/demo/config/WebSecurityConfig.java
+++ sapl-demo-mvc-app/src/main/java/org/demo/config/WebSecurityConfig.java
@@ -23,7 +23,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
