ceb690b9b8ededbb011df1e0ef662c91bc208898
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2022-06-15 17:58:24 +0200

    migrate security setting to spring boot 2.7.0 practices

--- sapl-demo-multi-tenant/src/main/java/org/demo/config/WebSecurityConfig.java
+++ sapl-demo-multi-tenant/src/main/java/org/demo/config/WebSecurityConfig.java
@@ -18,17 +18,16 @@ package org.demo.config;
 import org.demo.domain.DemoData;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
 	@Bean
 	public UserDetailsService userDetailsService() {
@@ -42,19 +41,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// @formatter:off
+		return http  .authorizeRequests()
+				     .anyRequest()
+				     .authenticated()
+			   .and().formLogin()
+				   	 .defaultSuccessUrl("/patients/1", true)
+			   .and().logout()
+				     .permitAll()
+				     .logoutSuccessUrl("/login")
+			   .and().csrf()
+			         .disable()
+			   .build();
+		// @formatter:on
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and()
-	    .formLogin().defaultSuccessUrl("/patients/1", true)
-	    .and()
-	    .logout().permitAll().logoutSuccessUrl("/login").and()
-				.csrf().disable();
-	}
-
-	
 }
