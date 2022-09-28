f256cedf37bc192b074a0d6fc4e05b2ec26a0a52
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2022-09-28 23:35:29 +0200

    bump Axon to 4.6.0

--- sapl-demo-axon/src/main/java/io/sapl/demo/axon/configuration/AxonMongoConfiguration.java
+++ sapl-demo-axon/src/main/java/io/sapl/demo/axon/configuration/AxonMongoConfiguration.java
@@ -4,14 +4,11 @@ import org.axonframework.eventhandling.tokenstore.TokenStore;
 import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.Snapshotter;
-import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
-import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
 import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
 import org.axonframework.serialization.Serializer;
-import org.axonframework.spring.config.AxonConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,12 +16,6 @@ import com.mongodb.client.MongoClient;
 
 @Configuration
 public class AxonMongoConfiguration {
-
-	@Bean
-	public EventStore eventStore(EventStorageEngine storageEngine, AxonConfiguration configuration) {
-		return EmbeddedEventStore.builder().storageEngine(storageEngine)
-				.messageMonitor(configuration.messageMonitor(EventStore.class, "eventStore")).build();
-	}
 
 	@Bean
 	public EventStorageEngine storageEngine(MongoClient client, Serializer serializer) {
