b2ab392e0a31b31dc19311b3f91e7d87ded9ad51
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2023-04-19 19:12:37 +0200

    fix JAR build of editor/playground demo

--- sapl-demo-playground/src/main/dev-bundle/webapp/VAADIN/build/component-picker-c2e3a4e9.js
+++ sapl-demo-playground/src/main/dev-bundle/webapp/VAADIN/build/component-picker-c2e3a4e9.js
@@ -1,4 +1,4 @@
-import{L as a,c as r,h as p,p as u,i as g,j as m,q as f}from"./indexhtml-a389c10d.js";/**
+import{L as a,c as r,h as p,p as u,i as g,j as m,q as f}from"./indexhtml-9a225186.js";/**
  * @license
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
