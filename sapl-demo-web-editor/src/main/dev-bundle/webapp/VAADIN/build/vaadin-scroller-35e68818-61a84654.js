71fbf9cac77aa2801a3cb0efb4d5711e43a486de
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2023-06-26 14:00:43 +0200

    Delete sapl-demo-web-editor/src/main/dev-bundle directory

--- sapl-demo-web-editor/src/main/dev-bundle/webapp/VAADIN/build/vaadin-scroller-35e68818-61a84654.js
+++
@@ -1,1 +0,0 @@
-import{U as r}from"./indexhtml-af09eb6f.js";const o={tagName:"vaadin-scroller",displayName:"Scroller",elements:[{selector:"vaadin-scroller",displayName:"Scroller",properties:[r.backgroundColor,r.borderColor,r.borderWidth,r.borderRadius,r.padding]}]};export{o as default};
