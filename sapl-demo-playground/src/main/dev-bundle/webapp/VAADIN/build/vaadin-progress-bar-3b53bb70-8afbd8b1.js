9c02f2674f360d492d667fc5bad8aaf347265ffc
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2023-06-26 14:00:20 +0200

    Delete sapl-demo-playground/src/main/dev-bundle directory

--- sapl-demo-playground/src/main/dev-bundle/webapp/VAADIN/build/vaadin-progress-bar-3b53bb70-8afbd8b1.js
+++
@@ -1,1 +0,0 @@
-import{U as r}from"./indexhtml-c7f759a4.js";const e={tagName:"vaadin-progress-bar",displayName:"ProgressBar",elements:[{selector:"vaadin-progress-bar::part(bar)",displayName:"Bar",properties:[r.backgroundColor]},{selector:"vaadin-progress-bar::part(value)",displayName:"Value",properties:[r.backgroundColor]}]};export{e as default};
