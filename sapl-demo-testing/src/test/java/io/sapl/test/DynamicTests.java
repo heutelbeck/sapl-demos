eae04f06c46fab410c0371240f31c7d7b33db16f
Alexander Dröfke <A.Droefke96@gmail.com>
2023-09-05 20:13:32 +0200

    adapt test execution to enable junit runner integration

---
+++ sapl-demo-testing/src/test/java/io/sapl/test/DynamicTests.java
@@ -0,0 +1,13 @@
+package io.sapl.test;
+
+import io.sapl.test.setup.TestBuilder;
+import java.util.Collection;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+public class DynamicTests {
+    @TestFactory
+    Collection<DynamicTest> saplDslTests() {
+        return TestBuilder.buildTests(new TestPIP(), "test.sapltest");
+    }
+}
