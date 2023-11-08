10921b2bde3e294975e5390bd9fb0a80c0bf2527
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2023-11-08 20:04:31 +0100

    match API changes in engine

--- sapl-demo-playground/src/main/java/io/sapl/playground/XtextServletConfiguration.java
+++ sapl-demo-playground/src/main/java/io/sapl/playground/XtextServletConfiguration.java
@@ -15,6 +15,16 @@
  */
 package io.sapl.playground;
 
+import java.time.Clock;
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.filter.OrderedFormContentFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 import io.sapl.functions.FilterFunctionLibrary;
 import io.sapl.functions.StandardFunctionLibrary;
 import io.sapl.functions.TemporalFunctionLibrary;
@@ -26,46 +36,38 @@ import io.sapl.interpreter.pip.AnnotationAttributeContext;
 import io.sapl.interpreter.pip.AttributeContext;
 import io.sapl.pip.TimePolicyInformationPoint;
 
-import java.time.Clock;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.servlet.filter.OrderedFormContentFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
 @Configuration
 @ComponentScan("io.sapl.grammar.ide.contentassist")
 public class XtextServletConfiguration {
 
-	@Bean
-	ServletRegistrationBean<SAPLServlet> xTextRegistrationBean() {
-		ServletRegistrationBean<SAPLServlet> registration = new ServletRegistrationBean<>(new SAPLServlet(),
-				"/xtext-service/*");
-		registration.setName("XtextServices");
-		registration.setAsyncSupported(true);
-		return registration;
-	}
+    @Bean
+    ServletRegistrationBean<SAPLServlet> xTextRegistrationBean() {
+        ServletRegistrationBean<SAPLServlet> registration = new ServletRegistrationBean<>(new SAPLServlet(),
+                "/xtext-service/*");
+        registration.setName("XtextServices");
+        registration.setAsyncSupported(true);
+        return registration;
+    }
 
-	@Bean
-	FilterRegistrationBean<OrderedFormContentFilter> registration1(OrderedFormContentFilter filter) {
-		FilterRegistrationBean<OrderedFormContentFilter> registration = new FilterRegistrationBean<>(filter);
-		registration.setEnabled(false);
-		return registration;
-	}
+    @Bean
+    FilterRegistrationBean<OrderedFormContentFilter> registration1(OrderedFormContentFilter filter) {
+        FilterRegistrationBean<OrderedFormContentFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setEnabled(false);
+        return registration;
+    }
 
-	@Bean
-	FunctionContext functionContext() throws InitializationException {
-		return new AnnotationFunctionContext(new FilterFunctionLibrary(),
-				new StandardFunctionLibrary(), new TemporalFunctionLibrary());
-	}
+    @Bean
+    FunctionContext functionContext() throws InitializationException {
+        var staticLibraries = List.of(FilterFunctionLibrary.class, StandardFunctionLibrary.class,
+                TemporalFunctionLibrary.class);
+        return new AnnotationFunctionContext(() -> List.of(), () -> staticLibraries);
+    }
 
-	@Bean
-	AttributeContext attributeContext() throws InitializationException {
-		AnnotationAttributeContext context = new AnnotationAttributeContext();
-		context.loadPolicyInformationPoint(new TimePolicyInformationPoint(Clock.systemUTC()));
-		return context;
-	}
+    @Bean
+    AttributeContext attributeContext() throws InitializationException {
+        AnnotationAttributeContext context = new AnnotationAttributeContext();
+        context.loadPolicyInformationPoint(new TimePolicyInformationPoint(Clock.systemUTC()));
+        return context;
+    }
 
 }
