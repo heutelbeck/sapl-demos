6038f2249e1b9d7479fa67791d4c6d381f1ad5ce
Dominic Heutelbeck <dheutelbeck@ftk.de>
2024-01-11 02:41:39 +0100

    Update MvcDemoApplicationTest.java

--- sapl-demo-mvc-app/src/test/java/io/sapl/mvc/demo/MvcDemoApplicationTest.java
+++ sapl-demo-mvc-app/src/test/java/io/sapl/mvc/demo/MvcDemoApplicationTest.java
@@ -16,6 +16,7 @@
 package io.sapl.mvc.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -28,8 +29,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
@@ -52,187 +53,183 @@ import lombok.SneakyThrows;
 @SpringBootTest(classes = MvcDemoApplication.class)
 class MvcDemoApplicationTest {
 
-	static record IntegerAndString(int integer, String string) {
-	}
+    static record IntegerAndString(int integer, String string) {
+    }
 
-	static Collection<String> userNameSource() {
-		return List.of(DemoData.USER_NAMES);
-	}
+    static Collection<String> userNameSource() {
+        return List.of(DemoData.USER_NAMES);
+    }
 
-	static Collection<String> doctorNameSource() {
-		return List.of(DemoData.DOCTOR_NAMES);
-	}
-	
-	static Collection<String> nonDoctorNameSource() {
-		return List.of(DemoData.NON_DOCTOR_NAMES);
-	}
+    static Collection<String> doctorNameSource() {
+        return List.of(DemoData.DOCTOR_NAMES);
+    }
 
-	static void assertPatientsList(HtmlPage page, IntegerAndString... idsAndNames) {
+    static Collection<String> nonDoctorNameSource() {
+        return List.of(DemoData.NON_DOCTOR_NAMES);
+    }
 
-		var tableRows = page.<HtmlTableRow>getByXPath("//table/tbody/tr");
-		assertThat(tableRows.size()).isEqualTo(idsAndNames.length + 1);
-		assertThat(tableRows.get(0).getCells().size()).isEqualTo(2);
-		assertThat(tableRows.get(0).getCell(0).getVisibleText()).isEqualTo("ID");
-		assertThat(tableRows.get(0).getCell(1).getVisibleText()).isEqualTo("NAME");
+    static void assertPatientsList(HtmlPage page, IntegerAndString... idsAndNames) {
 
-		var patientLinks = page.<HtmlAnchor>getByXPath("//table/tbody/tr/td[last()]/a");
-		assertThat(patientLinks.size()).isEqualTo(idsAndNames.length);
+        var tableRows = page.<HtmlTableRow>getByXPath("//table/tbody/tr");
+        assertThat(tableRows.size()).isEqualTo(idsAndNames.length + 1);
+        assertThat(tableRows.get(0).getCells().size()).isEqualTo(2);
+        assertThat(tableRows.get(0).getCell(0).getVisibleText()).isEqualTo("ID");
+        assertThat(tableRows.get(0).getCell(1).getVisibleText()).isEqualTo("NAME");
 
-		var index = 0;
-		for (var idAndName : idsAndNames) {
-			assertThat(patientLinks.get(index).getVisibleText()).isEqualTo(idAndName.string());
-			assertThat(patientLinks.get(index).getHrefAttribute()).isEqualTo("/patients/" + idAndName.integer());
-			index++;
-			assertThat(tableRows.get(index).getCells().size()).isEqualTo(2);
-			assertThat(tableRows.get(index).getCell(0).getVisibleText())
-					.isEqualTo(Integer.toString(idAndName.integer()));
-		}
-	}
+        var patientLinks = page.<HtmlAnchor>getByXPath("//table/tbody/tr/td[last()]/a");
+        assertThat(patientLinks.size()).isEqualTo(idsAndNames.length);
 
-	WebClient mockClient;
+        var index = 0;
+        for (var idAndName : idsAndNames) {
+            assertThat(patientLinks.get(index).getVisibleText()).isEqualTo(idAndName.string());
+            assertThat(patientLinks.get(index).getHrefAttribute()).isEqualTo("/patients/" + idAndName.integer());
+            index++;
+            assertThat(tableRows.get(index).getCells().size()).isEqualTo(2);
+            assertThat(tableRows.get(index).getCell(0).getVisibleText())
+                    .isEqualTo(Integer.toString(idAndName.integer()));
+        }
+    }
 
-	@Autowired
-	AuthenticationManagerBuilder authBuilder;
+    WebClient mockClient;
 
-	@Autowired
-	UIController uiController;
+    @Autowired
+    UserDetailsService userDetailsService;
 
-	@Autowired
-	MockMvc mockMvc;
+    @Autowired
+    UIController uiController;
 
-	@BeforeEach
-	void beforeEach() {
-		mockClient = MockMvcWebClientBuilder.mockMvcSetup(mockMvc).build();
-	}
+    @Autowired
+    MockMvc mockMvc;
 
-	@Test
-	void contextLoads(ApplicationContext context) {
-		assertThat(context).isNotNull();
-	}
+    @BeforeEach
+    void beforeEach() {
+        mockClient = MockMvcWebClientBuilder.mockMvcSetup(mockMvc).build();
+    }
 
-	@SneakyThrows
-	@ParameterizedTest
-	@MethodSource("userNameSource")
-	void when_home_then_showHome(String username) {
-		final String url = "http://localhost:8080/";
-		setAuthentication(username);
-		HtmlPage page = mockClient.getPage(url);
+    @Test
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
+    }
 
-		var header = page.<HtmlHeading1>getByXPath("//h1");
-		assertThat(header.size()).isEqualTo(1);
-		assertThat(header.get(0).getVisibleText()).isEqualTo("Hello " + username + "!");
+    @SneakyThrows
+    @ParameterizedTest
+    @MethodSource("userNameSource")
+    void when_home_then_showHome(String username) {
+        final String url = "http://localhost:8080/";
+        setAuthentication(username);
+        HtmlPage page = mockClient.getPage(url);
 
-		var patientLink = page.<HtmlAnchor>getByXPath("//h3/a");
-		assertThat(patientLink.size()).isEqualTo(1);
-		assertThat(patientLink.get(0).getVisibleText()).isEqualTo("Patient List");
-		assertThat(patientLink.get(0).getHrefAttribute()).isEqualTo("/patients");
+        var header = page.<HtmlHeading1>getByXPath("//h1");
+        assertThat(header.size()).isEqualTo(1);
+        assertThat(header.get(0).getVisibleText()).isEqualTo("Hello " + username + "!");
 
-		var logoutForm = page.<HtmlForm>getByXPath("//form");
-		assertThat(logoutForm.size()).isEqualTo(1);
-		assertThat(logoutForm.get(0).getActionAttribute()).isEqualTo("/logout");
-		assertThat(logoutForm.get(0).getMethodAttribute()).isEqualTo("post");
+        var patientLink = page.<HtmlAnchor>getByXPath("//h3/a");
+        assertThat(patientLink.size()).isEqualTo(1);
+        assertThat(patientLink.get(0).getVisibleText()).isEqualTo("Patient List");
+        assertThat(patientLink.get(0).getHrefAttribute()).isEqualTo("/patients");
 
-		var logoutSubmit = page.<HtmlInput>getByXPath("//form/input[@type='submit']");
-		assertThat(logoutSubmit.size()).isEqualTo(1);
-		assertThat(logoutSubmit.get(0).getValueAttribute()).isEqualTo("Sign Out");
+        var logoutForm = page.<HtmlForm>getByXPath("//form");
+        assertThat(logoutForm.size()).isEqualTo(1);
+        assertThat(logoutForm.get(0).getActionAttribute()).isEqualTo("/logout");
+        assertThat(logoutForm.get(0).getMethodAttribute()).isEqualTo("post");
 
-	}
+        var logoutSubmit = page.<HtmlInput>getByXPath("//form/input[@type='submit']");
+        assertThat(logoutSubmit.size()).isEqualTo(1);
+        assertThat(logoutSubmit.get(0).getValueAttribute()).isEqualTo("Sign Out");
 
-	@SneakyThrows
-	@ParameterizedTest
-	@MethodSource("doctorNameSource")
-	void when_patientsList_as_doctor_then_showPatientsList_and_showCreateButton(String username) {
-		final String url = "http://localhost:8080/patients";
-		setAuthentication(username);
-		HtmlPage page = mockClient.getPage(url);
+    }
 
-		var header = page.<HtmlHeading3>getByXPath("//h3");
-		assertThat(header.size()).isEqualTo(1);
-		assertThat(header.get(0).getVisibleText()).isEqualTo("List of Patients");
+    @SneakyThrows
+    @ParameterizedTest
+    @MethodSource("doctorNameSource")
+    void when_patientsList_as_doctor_then_showPatientsList_and_showCreateButton(String username) {
+        final String url = "http://localhost:8080/patients";
+        setAuthentication(username);
+        HtmlPage page = mockClient.getPage(url);
 
-		assertPatientsList(page, new IntegerAndString(1, "Lenny"), new IntegerAndString(2, "Karl"));
+        var header = page.<HtmlHeading3>getByXPath("//h3");
+        assertThat(header.size()).isEqualTo(1);
+        assertThat(header.get(0).getVisibleText()).isEqualTo("List of Patients");
 
-		var forms = page.<HtmlForm>getByXPath("//form");
-		assertThat(forms.size()).isEqualTo(2);
-		
-		var newPatientForm = page.<HtmlForm>getByXPath("//form[1]");
-		assertThat(newPatientForm.size()).isEqualTo(1);
-		assertThat(newPatientForm.get(0).getActionAttribute()).isEqualTo("/patients/new");
-		var newPatientFormInput = page.<HtmlInput>getByXPath("//form[1]/input");
-		assertThat(newPatientFormInput.size()).isEqualTo(1);
-		assertThat(newPatientFormInput.get(0).getTypeAttribute()).isEqualTo("submit");
-		assertThat(newPatientFormInput.get(0).getValueAttribute()).isEqualTo("Register new patient...");
-		
-		var homeForm = page.<HtmlForm>getByXPath("//form[last()]");
-		assertThat(homeForm.size()).isEqualTo(1);
-		assertThat(homeForm.get(0).getActionAttribute()).isEqualTo("/");
-		var homeFormInput = page.<HtmlInput>getByXPath("//form[last()]/input");
-		assertThat(homeFormInput.size()).isEqualTo(1);
-		assertThat(homeFormInput.get(0).getTypeAttribute()).isEqualTo("submit");
-		assertThat(homeFormInput.get(0).getValueAttribute()).isEqualTo("Home");
-	}
-	
-	@SneakyThrows
-	@ParameterizedTest
-	@MethodSource("nonDoctorNameSource")
-	void when_patientsList_as_nonDoctor_then_showPatientsList(String username) {
-		final String url = "http://localhost:8080/patients";
-		setAuthentication(username);
-		HtmlPage page = mockClient.getPage(url);
+        assertPatientsList(page, new IntegerAndString(1, "Lenny"), new IntegerAndString(2, "Karl"));
 
-		var header = page.<HtmlHeading3>getByXPath("//h3");
-		assertThat(header.size()).isEqualTo(1);
-		assertThat(header.get(0).getVisibleText()).isEqualTo("List of Patients");
+        var forms = page.<HtmlForm>getByXPath("//form");
+        assertThat(forms.size()).isEqualTo(2);
 
-		assertPatientsList(page, new IntegerAndString(1, "Lenny"), new IntegerAndString(2, "Karl"));
+        var newPatientForm = page.<HtmlForm>getByXPath("//form[1]");
+        assertThat(newPatientForm.size()).isEqualTo(1);
+        assertThat(newPatientForm.get(0).getActionAttribute()).isEqualTo("/patients/new");
+        var newPatientFormInput = page.<HtmlInput>getByXPath("//form[1]/input");
+        assertThat(newPatientFormInput.size()).isEqualTo(1);
+        assertThat(newPatientFormInput.get(0).getTypeAttribute()).isEqualTo("submit");
+        assertThat(newPatientFormInput.get(0).getValueAttribute()).isEqualTo("Register new patient...");
 
-		var forms = page.<HtmlForm>getByXPath("//form");
-		assertThat(forms.size()).isEqualTo(1);
-		
-		var homeForm = page.<HtmlForm>getByXPath("//form[last()]");
-		assertThat(homeForm.size()).isEqualTo(1);
-		assertThat(homeForm.get(0).getActionAttribute()).isEqualTo("/");
-		var homeFormInput = page.<HtmlInput>getByXPath("//form[last()]/input");
-		assertThat(homeFormInput.size()).isEqualTo(1);
-		assertThat(homeFormInput.get(0).getTypeAttribute()).isEqualTo("submit");
-		assertThat(homeFormInput.get(0).getValueAttribute()).isEqualTo("Home");
-	}
-	
-	@SneakyThrows
-	@ParameterizedTest
-	@MethodSource("doctorNameSource")
-	void when_newPatient_as_doctor_then__(String username) {
-		final String url = "http://localhost:8080/patients/new";
-		setAuthentication(username);
-		HtmlPage page = mockClient.getPage(url);
+        var homeForm = page.<HtmlForm>getByXPath("//form[last()]");
+        assertThat(homeForm.size()).isEqualTo(1);
+        assertThat(homeForm.get(0).getActionAttribute()).isEqualTo("/");
+        var homeFormInput = page.<HtmlInput>getByXPath("//form[last()]/input");
+        assertThat(homeFormInput.size()).isEqualTo(1);
+        assertThat(homeFormInput.get(0).getTypeAttribute()).isEqualTo("submit");
+        assertThat(homeFormInput.get(0).getValueAttribute()).isEqualTo("Home");
+    }
 
-		var header = page.<HtmlHeading3>getByXPath("//h3");
-		assertThat(header.size()).isEqualTo(1);
-		assertThat(header.get(0).getVisibleText()).isEqualTo("Create a new patient");
+    @SneakyThrows
+    @ParameterizedTest
+    @MethodSource("nonDoctorNameSource")
+    void when_patientsList_as_nonDoctor_then_showPatientsList(String username) {
+        final String url = "http://localhost:8080/patients";
+        setAuthentication(username);
+        HtmlPage page = mockClient.getPage(url);
 
-		var forms = page.<HtmlForm>getByXPath("//form");
-		assertThat(forms.size()).isEqualTo(2);
-		
-		var newPatientForm = page.<HtmlForm>getByXPath("//form[1]");
-		assertThat(newPatientForm.size()).isEqualTo(1);
-		
-		
-		
-		
-		
-		var homeForm = page.<HtmlForm>getByXPath("//form[last()]");
-		assertThat(homeForm.size()).isEqualTo(1);
-		assertThat(homeForm.get(0).getActionAttribute()).isEqualTo("/");
-		var homeFormInput = page.<HtmlInput>getByXPath("//form[last()]/input");
-		assertThat(homeFormInput.size()).isEqualTo(1);
-		assertThat(homeFormInput.get(0).getTypeAttribute()).isEqualTo("submit");
-		assertThat(homeFormInput.get(0).getValueAttribute()).isEqualTo("Back to Home");
-	}
+        var header = page.<HtmlHeading3>getByXPath("//h3");
+        assertThat(header.size()).isEqualTo(1);
+        assertThat(header.get(0).getVisibleText()).isEqualTo("List of Patients");
 
-	private void setAuthentication(String username) {
-		var user = authBuilder.getDefaultUserDetailsService().loadUserByUsername(username);
-		var authn = new UsernamePasswordAuthenticationToken(user, DemoData.DEFAULT_RAW_PASSWORD, user.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(authn);
-	}
+        assertPatientsList(page, new IntegerAndString(1, "Lenny"), new IntegerAndString(2, "Karl"));
+
+        var forms = page.<HtmlForm>getByXPath("//form");
+        assertThat(forms.size()).isEqualTo(1);
+
+        var homeForm = page.<HtmlForm>getByXPath("//form[last()]");
+        assertThat(homeForm.size()).isEqualTo(1);
+        assertThat(homeForm.get(0).getActionAttribute()).isEqualTo("/");
+        var homeFormInput = page.<HtmlInput>getByXPath("//form[last()]/input");
+        assertThat(homeFormInput.size()).isEqualTo(1);
+        assertThat(homeFormInput.get(0).getTypeAttribute()).isEqualTo("submit");
+        assertThat(homeFormInput.get(0).getValueAttribute()).isEqualTo("Home");
+    }
+
+    @SneakyThrows
+    @ParameterizedTest
+    @MethodSource("doctorNameSource")
+    void when_newPatient_as_doctor_then__(String username) {
+        final String url = "http://localhost:8080/patients/new";
+        setAuthentication(username);
+        HtmlPage page = mockClient.getPage(url);
+
+        var header = page.<HtmlHeading3>getByXPath("//h3");
+        assertThat(header.size()).isEqualTo(1);
+        assertThat(header.get(0).getVisibleText()).isEqualTo("Create a new patient");
+
+        var forms = page.<HtmlForm>getByXPath("//form");
+        assertThat(forms.size()).isEqualTo(2);
+
+        var newPatientForm = page.<HtmlForm>getByXPath("//form[1]");
+        assertThat(newPatientForm.size()).isEqualTo(1);
+
+        var homeForm = page.<HtmlForm>getByXPath("//form[last()]");
+        assertThat(homeForm.size()).isEqualTo(1);
+        assertThat(homeForm.get(0).getActionAttribute()).isEqualTo("/");
+        var homeFormInput = page.<HtmlInput>getByXPath("//form[last()]/input");
+        assertThat(homeFormInput.size()).isEqualTo(1);
+        assertThat(homeFormInput.get(0).getTypeAttribute()).isEqualTo("submit");
+        assertThat(homeFormInput.get(0).getValueAttribute()).isEqualTo("Back to Home");
+    }
+
+    private void setAuthentication(String username) {
+        var user  = userDetailsService.loadUserByUsername(username);
+        var authn = new UsernamePasswordAuthenticationToken(user, DemoData.DEFAULT_RAW_PASSWORD, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authn);
+    }
 
 }
