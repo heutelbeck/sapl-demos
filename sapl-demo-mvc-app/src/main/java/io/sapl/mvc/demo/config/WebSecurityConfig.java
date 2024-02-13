d6f75001e8388aa36106581b7e37ac8f812412af
Praktikant42 <145603357+Praktikant42@users.noreply.github.com>
2024-02-13 20:20:22 +0100

    Update WebSecurityConfig.java
    Update Argon2

--- sapl-demo-mvc-app/src/main/java/io/sapl/mvc/demo/config/WebSecurityConfig.java
+++ sapl-demo-mvc-app/src/main/java/io/sapl/mvc/demo/config/WebSecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -66,7 +66,7 @@ public class WebSecurityConfig {
 
     @Bean
     static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 
 }
