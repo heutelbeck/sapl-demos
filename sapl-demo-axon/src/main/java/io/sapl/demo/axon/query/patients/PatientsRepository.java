d2a54503cc45af664ff7178918e779c945d91c9b
Dominic Heutelbeck <dheutelbeck@ftk.de>
2024-02-29 19:48:53 +0100

    remove demos for extensions not in main engine repo

--- sapl-demo-axon/src/main/java/io/sapl/demo/axon/query/patients/PatientsRepository.java
+++
@@ -1,9 +0,0 @@
-package io.sapl.demo.axon.query.patients;
-
-import org.springframework.data.repository.CrudRepository;
-
-import io.sapl.demo.axon.query.patients.api.PatientDocument;
-
-public interface PatientsRepository extends CrudRepository<PatientDocument, String>{
-
-}
--- sapl-demo-axon/src/main/java/io/sapl/demo/axon/query/publisher/ReactivePatientsRepository.java
+++
@@ -1,8 +0,0 @@
-package io.sapl.demo.axon.query.publisher;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import io.sapl.demo.axon.query.patients.api.PatientDocument;
-
-public interface ReactivePatientsRepository extends ReactiveCrudRepository<PatientDocument, String> {
-}
